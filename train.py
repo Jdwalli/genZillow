f8aefa2665bf776884ec3e1e3330f6ccd5e8a76c
Your Name <jdwalli2003@yahoo.com>
2021-09-30 02:53:34 -0400

    adding base files

---
+++ train.py
@@ -0,0 +1,79 @@
+import random
+from tensorflow.keras.optimizers import SGD
+from keras.layers import Dense, Dropout
+from keras.models import load_model
+from keras.models import Sequential
+import numpy as np
+import pickle
+import json
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+def train_func():
+    lemmatizer = WordNetLemmatizer()
+    nltk.download("punkt")
+    nltk.download("wordnet")
+
+    training = []
+    words = []
+    classes = []
+    documents = []
+    ignore_words = ["?", "!"]
+
+    with open("tools/intents.json") as f:
+        intents = json.loads(f.read())
+
+    for intent in intents["intents"]:
+        for pattern in intent["patterns"]:
+            w = nltk.word_tokenize(pattern)
+            words.extend(w)
+            documents.append((w, intent["tag"]))
+            if intent["tag"] not in classes:
+                classes.append(intent["tag"])
+
+
+    words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+    words = sorted(list(set(words)))
+
+    classes = sorted(list(set(classes)))
+
+    pickle.dump(words, open("tools/words.pkl", "wb"))
+    pickle.dump(classes, open("tools/classes.pkl", "wb"))
+
+    output_empty = [0] * len(classes)
+    for doc in documents:
+        bag = []
+        pattern_words = doc[0]
+        pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+        for w in words:
+            bag.append(1) if w in pattern_words else bag.append(0)
+
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
+
+        training.append([bag, output_row])
+
+    random.shuffle(training)
+    training = np.array(training)
+
+    train_x = list(training[:, 0])
+    train_y = list(training[:, 1])
+
+
+    model = Sequential()
+    model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_y[0]), activation="softmax"))
+    model.summary()
+
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
+
+
+
+    hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+    model.save("tools/chatbot_model.h5", hist)
+
+    
